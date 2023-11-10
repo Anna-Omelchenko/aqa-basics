17efbe17ba680d2efc4276318a7622fb13ab188d
Anna Omelchenko <anna.g.omelchenko@gmail.com>
2023-11-10 20:14:45 +0300

    TestNG unit tests for CashbackHackService

---
+++ src/test/java/ru/netology/cashback/CashbackHackServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.cashback;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void testCommonCase() {
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(service.remain(1705), 295);
+        Assert.assertEquals(service.remain(880), 120);
+        Assert.assertEquals(service.remain(10), 990);
+    }
+
+    @Test
+    public void testRoundAmount() {
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(service.remain(1000), 0);
+        Assert.assertEquals(service.remain(20000), 0);
+    }
+}
